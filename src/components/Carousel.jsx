77828f055003ba56debfc72a670cdae051b6f985
Ledja Halltari <l.halltari@teamsystem.com>
2023-10-21 14:32:26 +0200

    Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,22 +1,67 @@
 import React from 'react';
-import { Card, Box, Typography } from '@mui/material';
-const Carousel = () => {
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 4 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 3,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 464 },
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
+  }
+};
+const sliderImageUrl = [
+  //First image url
+  {
+    url: 'https://i2.wp.com/www.geeksaresexy.net/wp-content/uploads/2020/04/movie1.jpg?resize=600%2C892&ssl=1'
+  },
+  {
+    url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-kids-movies-2020-call-of-the-wild-1579042974.jpg?crop=0.9760858955588091xw:1xh;center,top&resize=480:*'
+  },
+  //Second image url
+  {
+    url: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*'
+  },
+  //Third image url
+  {
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQS82ET2bq9oTNwPOL8gqyoLoLfeqJJJWJmKQ&usqp=CAU'
+  },
+
+  //Fourth image url
+
+  {
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTdvuww0JDC7nFRxiFL6yFiAxRJgM-1tvJTxA&usqp=CAU'
+  }
+];
+const CarouselComponent = () => {
   return (
-    <div>
-      <Card orientation="horizontal" size="sm" key={item.title} variant="outlined">
-        {/*<AspectRatio ratio="1" sx={{ minWidth: 100 }}>*/}
-        {/*  <img*/}
-        {/*    srcSet={`${item.src}?h=120&fit=crop&auto=format&dpr=2 2x`}*/}
-        {/*    src={`${item.src}?h=120&fit=crop&auto=format`}*/}
-        {/*    alt={item.title}*/}
-        {/*  />*/}
-        {/*</AspectRatio>*/}
-        <Box sx={{ whiteSpace: 'nowrap', mx: 1 }}>
-          <Typography level="title-md">{item.title}</Typography>
-          <Typography level="body-sm">{item.description}</Typography>
-        </Box>
-      </Card>
+    <div className="parent">
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        infinite={true}
+        partialVisible={false}
+        dotListClass="custom-dot-list-style">
+        {sliderImageUrl.map((imageUrl, index) => {
+          return (
+            <div className="slider" key={index}>
+              <img src={imageUrl.url} alt="movie" />
+            </div>
+          );
+        })}
+      </Carousel>
     </div>
   );
 };
-export default Carousel;
+export default CarouselComponent;
--- src/pages/TestingCards.jsx
+++ src/pages/TestingCards.jsx
@@ -3,40 +3,47 @@ import WizzAir from '../assets/images/WizzAir.svg';
 import AirAlbania from '../assets/images/AirAlbania.svg';
 import Alitalia from '../assets/images/Alitalia.svg';
 import Landways from '../assets/images/Landways.png';
-import Carousel from '../components/Carousel';
+import CarouselComponent from '../components/Carousel';
 
 const TestingCards = () => {
   return (
-    <div className="flex space-x-6">
-      <Carousel />
-      <Card
-        imageSrc={WizzAir}
-        date="3 shkurt "
-        title="Latest Wizz news"
-        description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
-        onClick={() => console.log('testing')}
-      />
-      <Card
-        imageSrc={AirAlbania}
-        date="3 shkurt "
-        title="Latest Wizz news"
-        description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
-        onClick={() => console.log('testing')}
-      />
-      <Card
-        imageSrc={Alitalia}
-        date="3 shkurt "
-        title="Latest Wizz news"
-        description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
-        onClick={() => console.log('testing')}
-      />
-      <Card
-        imageSrc={Landways}
-        date="3 shkurt "
-        title="Latest Wizz news"
-        description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
-        onClick={() => console.log('testing')}
-      />
+    <div className="flex flex-col">
+      <section
+        style={{
+          margin: '20px 0 20px 0'
+        }}>
+        <CarouselComponent />
+      </section>
+      <div className="flex flex-row space-x-6">
+        <Card
+          imageSrc={WizzAir}
+          date="3 shkurt "
+          title="Latest Wizz news"
+          description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
+          onClick={() => console.log('testing')}
+        />
+        <Card
+          imageSrc={AirAlbania}
+          date="3 shkurt "
+          title="Latest Wizz news"
+          description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
+          onClick={() => console.log('testing')}
+        />
+        <Card
+          imageSrc={Alitalia}
+          date="3 shkurt "
+          title="Latest Wizz news"
+          description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
+          onClick={() => console.log('testing')}
+        />
+        <Card
+          imageSrc={Landways}
+          date="3 shkurt "
+          title="Latest Wizz news"
+          description="cHECK OUT HERE THE LATEST NEWS FROM WIZZAIR"
+          onClick={() => console.log('testing')}
+        />
+      </div>
     </div>
   );
 };
